48dfeac62aea75918102f8cec75b2684412d337b
Pete Cowles <kilgore9trout@gmail.com>
2021-04-01 19:57:13 -0700

    Blue Ocean - Project Setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: [
+                ['@babel/transform-runtime'],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};

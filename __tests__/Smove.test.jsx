efaa3a245bebd05bd3a617638e30047c9b7fea3f
Jesse <jesseholden713@gmail.com>
2021-04-05 16:32:12 -0700

    Implemented basic test

--- __tests__/Smove.test.jsx
+++ __tests__/Smove.test.jsx
@@ -2,3 +2,10 @@ import React from 'react';
 import App from '../client/components/App.jsx';
 import Login from '../client/components/Login.jsx';
 import { render, screen, cleanup, fireEvent, queryByTestId, act, queryByPlaceholderText } from '@testing-library/react';
+
+
+describe('A basic test', () => {
+  it('should add 2 numbers together', () => {
+    expect(2 + 2).toBe(4);
+  });
+});

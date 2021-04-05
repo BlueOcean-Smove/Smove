80c9f8b64bafd601654ea02a0591e478d036491d
Ming <mingwen314@gmail.com>
2021-04-05 09:49:55 -0700

    tested mapAPI and geoLocationAPI

---
+++ client/components/LocalBusiness.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MapContainer from './MapContainer';
+
+const BusinessMap = () => {
+
+
+  return (
+    <div>
+      <h2>Search for Business </h2>
+      <MapContainer />
+
+      </div>
+  )
+}
+
+export default BusinessMap;

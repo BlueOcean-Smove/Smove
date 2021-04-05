4cd2e7c55b0fc301b805c6f5865f98b93f0cffec
Dani Tian <td.tiandani@gmail.com>
2021-04-05 11:37:37 -0700

    added login component and removed archived routes

--- server/routes.js
+++ server/routes.js
@@ -7,39 +7,11 @@ const {
   changeAccess
 } = require('./controllers/users.js');
 
-const {
-  getSmove,
-  createSmove
-} = require('./controllers/smove.js');
-
-const {
-  createList,
-  updateList,
-  deleteList
-} = require('./controllers/lists.js');
-
-const {
-  createTask,
-  updateTask,
-  deleteTask
-} = require('./controllers/tasks.js');
-
 const router = express.Router();
 
 router.get('/user/:id', getUserInfo);
 router.post('/user', createUser);
 router.patch('/user/:id', updateUser);
 router.patch('/user/:id/:id', changeAccess);
-
-router.get('/:id', getSmove);
-router.post('/', createSmove);
-
-router.post('/list', createList);
-router.patch('/list/:id', updateList);
-router.delete('/list/:id', deleteList);
-
-router.post('/task', createTask);
-router.patch('/task/:id', updateTask);
-router.delete('/task/:id', deleteTask);
 
 module.exports = router;

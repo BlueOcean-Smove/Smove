7421812e86f429c7241027855a676cfaf49dde36
Pete Cowles <kilgore9trout@gmail.com>
2021-04-05 12:20:58 -0700

    Adding Mongo Schema

--- database/users.js
+++ database/users.js
@@ -1,9 +1,66 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-
+  googleID: {
+    type: String,
+    required: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  image: String,
+  zipcode: Number,
+  onSmoveTeam: [String],
+  smoves: [
+    {
+      smoveName: String,
+      isCurrentSmove: Boolean,
+      oldAddress: String,
+      newAddress: String,
+      favCompanies: [
+        {
+          companyName: String,
+          url: String,
+        }
+      ],
+      tasks: [
+        {
+          taskName: String,
+          location: String,
+          startDate: String,
+          endDate: String,
+          assignedTo: [String],
+          category: String,
+          status: String,
+          company: {
+            companyName: String,
+            url: String,
+          }
+        }
+      ],
+      inventory: [
+        {
+          boxNum: Number,
+          originRoom: String,
+          destinationRoom: String,
+          boxPriority: String,
+          notes: String,
+        }
+      ],
+      moveTeam: [String],
+    }
+  ],
 });
 
-const Users = mongoose.model('Users', userSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports.Users = Users;
+module.exports.User = User;

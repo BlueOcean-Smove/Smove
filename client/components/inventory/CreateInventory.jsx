125ca2ba3782069e4f7993000844ec941740baf8
Greta Schock <gschockcodes@gmail.com>
2021-04-05 20:39:08 -0700

    Added initial functionality for the modal

--- client/components/inventory/CreateInventory.jsx
+++ client/components/inventory/CreateInventory.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal'
 
 const CreateInventory = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
     return (
-        <div>Create Inventory is working!</div>
-    )
+      <React.Fragment>
+        <Button variant="primary" onClick={handleShow}>Add to Inventory</Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add New Inventory Item to List</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <div>Display Box Number Here</div>
+              <form>
+                <label for="fname">First name:</label>
+                <input type="text" id="fname" name="fname" value="John"></input>
+                <label for="lname">Last name:</label>
+                <input type="text" id="lname" name="lname" value="Doe"></input>
+              </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </React.Fragment>
+    );
 }
 
 export default CreateInventory;

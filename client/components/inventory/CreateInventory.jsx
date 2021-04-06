4eb6da33bd12c523a15f85cfd6a4be5663832073
Greta Schock <gschockcodes@gmail.com>
2021-04-06 14:43:11 -0700

    Added Forms to Create Inventory

--- client/components/inventory/CreateInventory.jsx
+++ client/components/inventory/CreateInventory.jsx
@@ -15,37 +15,38 @@ const CreateInventory = () => {
             <Button variant="primary" onClick={handleShow}>Add to Inventory</Button>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Add New Inventory Item to List</Modal.Title>
+                    <Modal.Title>Add New Inventory Box to List</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-    </Form.Text>
+                        <Form.Group controlId="formOriginRoom">
+                            <Form.Label>Origin Room</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Origin Room Name" />
+                            <Form.Text className="text-muted">Examples: Kitchen, Office, Living Room, etc.</Form.Text>
                         </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                        <Form.Group controlId="formDestinationRoom">
+                            <Form.Label>Destination Room</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Destination Room Name" />
+                            <Form.Text className="text-muted">Examples: Kitchen, Office, Living Room, etc.</Form.Text>
                         </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Group controlId="formPrioritySelect">
+                            <Form.Label>Priority Level</Form.Label>
+                            <Form.Control as="select">
+                                <option>Normal</option>
+                                <option>Urgent</option>
+                                <option>Low</option>
+                            </Form.Control>
                         </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-  </Button>
+                        <Form.Group controlId="formNotes">
+                            <Form.Label>Additional Notes</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Notes" />
+                            <Form.Text className="text-muted">Examples: Fragile, Contains Silverware, etc.</Form.Text>
+                        </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-            </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-            </Button>
+                    <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+                    <Button variant="primary" onClick={handleClose}>Save New Inventory Box</Button>
                 </Modal.Footer>
             </Modal>
         </React.Fragment>

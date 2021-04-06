0bfc573ae3796df3f2ea434fc01fb74d8450d6cd
johnkwak08 <junyoung.kwak@yahoo.com>
2021-04-05 20:19:08 -0700

    pulling new

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,26 +6,30 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header.jsx';
 import Inventory from './inventory/Inventory.jsx';
 import Tasks from './Tasks/TaskList.jsx';
+import { UserDataProvider } from './login/Login.jsx';
 
 // import component files to use for routes
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-      <Header />
-      <Login />
-      <Logout />
-      {/* <Route exact path="/" component={ Add in your component file } />
-      <Route exact path="/TaskList" component={ Add in your component file } /> */}
-      <Route exact path="/Inventory" component={Inventory} />
-      {/* <Route exact path="/LocalMovingServices" component={ Add in your component file } />
-      {/* <Route exact path="/" component={ Add in your component file } /> */}
-      <Route exact path="/TaskList" component={Tasks} />
-      {/* <Route exact path="/Inventory" component={ Add in your component file } /> */}
-      <Route exact path="/LocalMovingServices" component={ LocalMovingServices} />
-      {/* <Route exact path="/Login" component={ Add in your component file } /> */}
-      </BrowserRouter>
+      <UserDataProvider>
+        <h1>Hello World!</h1>
+        <Login />
+        <Logout />
+        <BrowserRouter>
+        <Header />
+        {/* <Route exact path="/" component={ Add in your component file } />
+        <Route exact path="/TaskList" component={ Add in your component file } /> */}
+        <Route exact path="/Inventory" component={Inventory} />
+        {/* <Route exact path="/LocalMovingServices" component={ Add in your component file } />
+        {/* <Route exact path="/" component={ Add in your component file } /> */}
+        <Route exact path="/TaskList" component={Tasks} />
+        {/* <Route exact path="/Inventory" component={ Add in your component file } />
+        <Route exact path="/LocalMovingServices" component={ Add in your component file } />
+        <Route exact path="/Login" component={ Add in your component file } /> */}
+        </BrowserRouter>
+      </UserDataProvider>
     </div>
   )
 }
--- client/components/Tasks/Tasks.jsx
+++ client/components/Tasks/Tasks.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { UserData } from '../login/Login.jsx';
 
 const Tasks = ({task}) => {
   const [selected, setSelected] = useState(false);
   const [complete, setComplete] = useState(false);
-  console.log(task);
+//   console.log(task);
+  const { UserData } = useContext(UserData);
+
 
 //   const assignedUsers = () => {
 //     if (task.assignedTo.length !== 0 ) {

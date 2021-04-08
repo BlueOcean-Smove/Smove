df20075358f1c204f9738eef4441582663630d6f
Dani Tian <td.tiandani@gmail.com>
2021-04-07 17:43:15 -0700

    logout button now works when logging out from any component

--- client/components/login/Logout.jsx
+++ client/components/login/Logout.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
 
 const Logout = () => {
+  const [loggedOut, setLoggedOut] = useState(false);
 
+  const history = useHistory();
   const onSignOut = (googleUser) => {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(() => {
       console.log('signed out');
     });
+    // setLoggedOut(true);
+    history.push('/');
   }
 
   return (

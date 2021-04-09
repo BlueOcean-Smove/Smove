07041ec60bdb0e071b59cac1fa052d97b2e8831b
Greta Schock <gschockcodes@gmail.com>
2021-04-08 21:17:24 -0700

    Layout changes

--- client/components/inventory/Inventory.jsx
+++ client/components/inventory/Inventory.jsx
@@ -2,12 +2,24 @@ import React, { useState, useContext } from 'react';
 import CreateInventory from './CreateInventory.jsx';
 import InventoryTable from './InventoryTable.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Inventory = () => {
     return (
         <div id="inventory">
-            <CreateInventory />
-            <InventoryTable />
+            {/* <CreateInventory /> */}
+            {/* <InventoryTable /> */}
+            <Container>
+                <Row>
+                    <Col sm={1}></Col>
+                    <Col>
+                        <h2>Inventory</h2>   
+                        <CreateInventory />
+                        <InventoryTable />
+                    </Col>
+                    <Col sm={1}></Col>
+                </Row>
+            </Container>
         </div>
     )
 }
